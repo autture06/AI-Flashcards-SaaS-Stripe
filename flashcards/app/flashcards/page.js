dcfd280fb6c4a3e6b0f6b3dfd7134c0401440bb7
autture06 <arya.utture@gmail.com>
2024-08-16 10:44:08 -0400

    flashcards page updated

--- flashcards/app/flashcards/page.js
+++ flashcards/app/flashcards/page.js
@@ -1,10 +1,22 @@
 'use client'
 import { useUser } from "@clerk/nextjs"
 import { useEffect } from "react"
+import { collection, doc } from "firebase/firestore"
 
 export default function Flashcards() {
     const {isLoaded, isSignedIn, user} = useUser()
-    useEffect(() => {}, [user])
+    useEffect(() => {
+        async function getFlashcards() {
+            //Check to see if document exists
+            const docRef = doc(collection(firestore, 'users'), user.id)
+            if(docSnap.exists()) {
+                console.log('User exists')
+            }
+            else {
+                await setDoc(docRef, {flashcards: []})
+            }
+        }
+    }, [user])
 
     //In case te user signs out while on the page
     if(!isLoaded || !isSignedIn) {
