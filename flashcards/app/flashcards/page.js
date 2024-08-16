a21018de3c3b463f4ad3bca107d236e3665195c5
autture06 <arya.utture@gmail.com>
2024-08-16 10:30:34 -0400

    flashcards page updated

--- flashcards/app/flashcards/page.js
+++ flashcards/app/flashcards/page.js
@@ -1,13 +1,17 @@
 'use client'
 import { useUser } from "@clerk/nextjs"
+import { useEffect } from "react"
 
 export default function Flashcards() {
     const {isLoaded, isSignedIn, user} = useUser()
+    useEffect(() => {}, [user])
 
     //In case te user signs out while on the page
     if(!isLoaded || !isSignedIn) {
         return null
     }
+
+    
 
     console.log(user.id) 
 
