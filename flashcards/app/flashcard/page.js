7bb67850d5555f574ca768b2b39f5cc4b7bc4456
autture06 <arya.utture@gmail.com>
2024-08-16 17:37:20 -0400

    flashcard page updated

--- flashcards/app/flashcard/page.js
+++ flashcards/app/flashcard/page.js
@@ -1,14 +1,16 @@
+'use client'
 import { useUser } from "@clerk/nextjs"
 import { useEffect, useState } from "react"
 import { collection, doc, getDoc, setDoc } from "firebase/firestore"
 import db from "../../firebase"
 import {Container, Grid, Card, CardActionArea, CardContent, Typography} from '@mui/material'
-import {useRouter} from 'next/navigation'
+import {useRouter, useSearchParams} from 'next/navigation'
 
 export default function Flashcard() {
     //Get query parameter
-    const router = useRouter()
-    const {id} = router.query
-    console.log(id)
+    const searchParams = useSearchParams()
+    const search = searchParams.get('id')
+    
+    useEffect(() => {}, [search])
     return <></>
 }
