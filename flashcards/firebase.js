c48ff4f70cdb3b1c0135d2fa204f5136fa8482b4
autture06 <arya.utture@gmail.com>
2024-08-17 13:53:11 -0400

    firebase updated

--- flashcards/firebase.js
+++ flashcards/firebase.js
@@ -7,12 +7,6 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyDbVgtuFU7pLl928ERLj9NHQdOcUMk2lCM",
-  // authDomain: "flashcards-cc70f.firebaseapp.com",
-  // projectId: "flashcards-cc70f",
-  // storageBucket: "flashcards-cc70f.appspot.com",
-  // messagingSenderId: "528732632644",
-  // appId: "1:528732632644:web:1f06430d693feeb955ee11"
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
