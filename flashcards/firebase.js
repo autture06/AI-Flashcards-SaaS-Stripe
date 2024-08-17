339dc135534d1943f39b7e459e361231a8090652
giuliobarde <giuliobarde@gmail.com>
2024-08-17 10:57:27 -0400

    firebse .env update

--- flashcards/firebase.js
+++ flashcards/firebase.js
@@ -7,12 +7,18 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDbVgtuFU7pLl928ERLj9NHQdOcUMk2lCM",
-  authDomain: "flashcards-cc70f.firebaseapp.com",
-  projectId: "flashcards-cc70f",
-  storageBucket: "flashcards-cc70f.appspot.com",
-  messagingSenderId: "528732632644",
-  appId: "1:528732632644:web:1f06430d693feeb955ee11"
+  // apiKey: "AIzaSyDbVgtuFU7pLl928ERLj9NHQdOcUMk2lCM",
+  // authDomain: "flashcards-cc70f.firebaseapp.com",
+  // projectId: "flashcards-cc70f",
+  // storageBucket: "flashcards-cc70f.appspot.com",
+  // messagingSenderId: "528732632644",
+  // appId: "1:528732632644:web:1f06430d693feeb955ee11"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
